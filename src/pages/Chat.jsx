e52b6c7642029594146fd677c758ef2c6934291f
FrostCube <nithert25@gmail.com>
2026-05-17 13:09:36 -0400

    Change text color on Chat page

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,6 +1,6 @@
 export default function Chat() {
   return (
-    <div className="text-5xl font-black text-white">
+    <div className="text-5xl font-black text-[#111827]">
       Chat Page Working
     </div>
   )
