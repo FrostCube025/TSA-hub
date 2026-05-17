c4589e946ac6a729e2352ee55b9c9b39ff499f7b
FrostCube <nithert25@gmail.com>
2026-05-17 13:09:15 -0400

    Change text color from white to dark gray

--- src/pages/Assignments.jsx
+++ src/pages/Assignments.jsx
@@ -1,6 +1,6 @@
 export default function Assignments() {
   return (
-    <div className="text-5xl font-black text-white">
+    <div className="text-5xl font-black text-[#111827]">
       Assignments Page Working
     </div>
   )
